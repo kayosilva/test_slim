50366157cf6dd9f4f2fab2693e03d8f34cac9a12
Kayo Silva <kayo.silva@unyleya.com.br>
2016-08-31 18:01:39 -0300

    Criando classes com dados constantes.

---
+++ app/src/Constants/GeneroConstants.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: kayosilva
+ * Date: 31/08/16
+ * Time: 2:26 PM
+ */
+
+namespace App\Constants;
+
+
+class GeneroConstants
+{
+    const MACHO = "M";
+    const FEMEA = "F";
+
+}
---
+++ app/src/Constants/StatusConstants.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Constants;
+
+
+class StatusConstants
+{
+
+    const VIVO = 1;
+    const MORTO = 0;
+}
