d1dee7a65934e2059707476af52a794f2c06d363
Kayo Silva <kayo.silva@unyleya.com.br>
2016-08-31 19:20:56 -0300

    alteracao na rota, nao finalizado.

--- app/routes.php
+++ app/routes.php
@@ -5,7 +5,10 @@ $app->get('/', function ($request, $response, $args) {
     return $this->renderer->render($response, 'index.phtml', $args);
 });
 
-$app->get('/users', 'App\Action\UserAction:getAll');
+$app->get('/api/focas', 'App\Action\FocaAction:getAll');
+
+//todo mudar para post
+$app->get('/api/focas/create', 'App\Action\FocaAction:create');
 
 //$app->get('/[{name}]', function ($request, $response, $args) {
 //    // Sample log message
