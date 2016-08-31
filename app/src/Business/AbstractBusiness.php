ef21afdf0555bfb07e1e783dbdfdd4908b49c12e
Kayo Silva <kayo.silva@unyleya.com.br>
2016-08-31 18:02:26 -0300

    Criando funcao para formatar a string da data em datime.

--- app/src/Business/AbstractBusiness.php
+++ app/src/Business/AbstractBusiness.php
@@ -19,4 +19,31 @@ abstract class AbstractBusiness
     {
         $this->_em = $entityManager;
     }
+
+
+    /**
+     * @param $data
+     * @return \DateTime
+     */
+    public function converteDataBanco($data)
+    {
+        if ($data) {
+
+            $data = date('d/m/Y H:i:s', strtotime($data));
+
+            $datetime = new \DateTime();
+            $arrDateTime = explode(' ', $data);
+            if (array_key_exists(1, $arrDateTime)) {
+                $arrTime = explode(':', $arrDateTime[1]);
+                $datetime->setTime($arrTime[0], $arrTime[1], $arrTime[2]);
+            }
+            $arrDate = explode('/', $arrDateTime[0]);
+            $datetime->setDate($arrDate[2], $arrDate[1], $arrDate[0]);
+            //se tipo = 1 vai colocar a hora como 00:00:00, se 2 vai colocar como 23:59:59
+            //utilizada para colocar a data inicial do dia e a data final
+        }
+        return $datetime;
+    }
+
+
 }
